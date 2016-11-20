64ae5d63859228486a66bd070f77b3c3b7c42e52
Titus Wormer <tituswormer@gmail.com>
2016-11-20 21:01:26 +0100

    Refactor code-style

--- test.js
+++ test.js
@@ -1,122 +1,101 @@
-/**
- * @author Titus Wormer
- * @copyright 2015 Titus Wormer
- * @license MIT
- * @module unist:util:modify-children
- * @fileoverview Test suite for `unist-util-modify-children`.
- */
-
 'use strict';
 
-/* eslint-env node, mocha */
-
-/*
- * Dependencies.
- */
+/* eslint-env mocha */
 
 var assert = require('assert');
 var modifyChildren = require('./');
-
-/*
- * Methods.
- */
 
 var throws = assert.throws;
 var equal = assert.strictEqual;
 var deepEqual = assert.deepEqual;
 
-/**
- * No-op.
- */
-function noop() {}
-
-noop();
-
-/*
- * Tests.
- */
+var noop = Function.prototype;
 
 describe('modifyChildren()', function () {
-    it('should throw when no `parent` is given', function () {
-        throws(function () {
-            modifyChildren(noop)();
-        }, /Missing children in `parent`/);
+  it('should throw when no `parent` is given', function () {
+    throws(
+      function () {
+        modifyChildren(noop)();
+      },
+      /Missing children in `parent`/
+    );
 
-        throws(function () {
-            modifyChildren(noop)({});
-        }, /Missing children in `parent`/);
-    });
+    throws(
+      function () {
+        modifyChildren(noop)({});
+      },
+      /Missing children in `parent`/
+    );
+  });
 
-    it('should invoke `fn` for each child in `parent`', function () {
-        var values = [0, 1, 2, 3];
-        var context = {};
-        var n = -1;
+  it('should invoke `fn` for each child in `parent`', function () {
+    var values = [0, 1, 2, 3];
+    var context = {};
+    var n = -1;
 
-        context.children = values;
+    context.children = values;
 
-        modifyChildren(function (child, index, parent) {
-            n++;
-            equal(child, values[n]);
-            equal(index, n);
-            equal(parent, context);
-        })(context);
-    });
+    modifyChildren(function (child, index, parent) {
+      n++;
+      equal(child, values[n]);
+      equal(index, n);
+      equal(parent, context);
+    })(context);
+  });
 
-    it('should work when new children are added', function () {
-        var values = [0, 1, 2, 3, 4, 5, 6];
-        var n = -1;
+  it('should work when new children are added', function () {
+    var values = [0, 1, 2, 3, 4, 5, 6];
+    var n = -1;
 
-        modifyChildren(function (child, index, parent) {
-            n++;
+    modifyChildren(function (child, index, parent) {
+      n++;
 
-            if (index < 3) {
-                parent.children.push(parent.children.length);
-            }
+      if (index < 3) {
+        parent.children.push(parent.children.length);
+      }
 
-            equal(child, values[n]);
-            equal(index, values[n]);
-        })({
-            'children': [0, 1, 2, 3]
-        });
-    });
+      equal(child, values[n]);
+      equal(index, values[n]);
+    })({children: [0, 1, 2, 3]});
+  });
 
-    it('should skip forwards', function () {
-        var values = [0, 1, 2, 3];
-        var n = -1;
-        var context = {};
+  it('should skip forwards', function () {
+    var values = [0, 1, 2, 3];
+    var n = -1;
+    var context = {};
 
-        context.children = [0, 1, 3];
+    context.children = [0, 1, 3];
 
-        modifyChildren(function (child, index, parent) {
-            equal(child, values[++n]);
+    modifyChildren(function (child, index, parent) {
+      equal(child, values[++n]);
 
-            if (child === 1) {
-                parent.children.splice(index + 1, 0, 2);
-                return index + 1;
-            }
-        })(context);
+      if (child === 1) {
+        parent.children.splice(index + 1, 0, 2);
+        return index + 1;
+      }
+    })(context);
 
-        deepEqual(context.children, values);
-    });
+    deepEqual(context.children, values);
+  });
 
-    it('should skip backwards', function () {
-        var invocations = [0, 1, -1, 0, 1, 2, 3];
-        var n = -1;
-        var context = {};
-        var inserted;
+  it('should skip backwards', function () {
+    var invocations = [0, 1, -1, 0, 1, 2, 3];
+    var n = -1;
+    var context = {};
+    var inserted;
 
-        context.children = [0, 1, 2, 3];
+    context.children = [0, 1, 2, 3];
 
-        modifyChildren(function (child, index, parent) {
-            equal(child, invocations[++n]);
+    modifyChildren(function (child, index, parent) {
+      equal(child, invocations[++n]);
 
-            if (!inserted && child === 1) {
-                inserted = true;
-                parent.children.unshift(-1);
-                return -1;
-            }
-        })(context);
+      if (!inserted && child === 1) {
+        inserted = true;
+        parent.children.unshift(-1);
+        return -1;
+      }
+    })(context);
 
-        deepEqual(context.children, [-1, 0, 1, 2, 3]);
-    });
+    deepEqual(context.children, [-1, 0, 1, 2, 3]);
+  });
 });
