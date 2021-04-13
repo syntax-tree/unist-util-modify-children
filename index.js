c206b25f96a204ff6a5de8da6c56ec6a80407dba
Titus Wormer <tituswormer@gmail.com>
2021-04-13 12:11:09 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 import {arrayIterate} from 'array-iterate'
 
-// Turn `callback` into a child-modifier accepting a parent.  See
-// `array-iterate` for more info.
+/**
+ * @typedef {import('unist').Parent} Parent
+ * @typedef {import('unist').Node} Node
+ *
+ * @callback Modifier
+ * @param {Node} node
+ * @param {number} index
+ * @param {Parent} parent
+ * @returns {number|void}
+ *
+ * @callback Modify
+ * @param {Parent} node
+ * @returns {void}
+ */
+
+/**
+ * Turn `callback` into a child-modifier accepting a parent.
+ * See `array-iterate` for more info.
+ *
+ * @param {Modifier} callback
+ * @returns {Modify}
+ */
 export function modifyChildren(callback) {
   return iterator
 
+  /**
+   * @param {Parent} parent
+   * @returns {void}
+   */
   function iterator(parent) {
     if (!parent || !parent.children) {
       throw new Error('Missing children in `parent` for `modifier`')
@@ -13,8 +37,14 @@ export function modifyChildren(callback) {
     arrayIterate(parent.children, iteratee, parent)
   }
 
-  // Pass the context as the third argument to `callback`.
-  function iteratee(value, index) {
-    return callback(value, index, this)
+  /**
+   * Pass the context as the third argument to `callback`.
+   *
+   * @this {Parent}
+   * @param {Node} node
+   * @param {number} index
+   */
+  function iteratee(node, index) {
+    return callback(node, index, this)
   }
 }
--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 import test from 'tape'
 import {modifyChildren} from './index.js'
 
-var noop = Function.prototype
+function noop() {}
 
 test('modifyChildren()', function (t) {
   t.throws(
     function () {
+      // @ts-ignore runtime.
       modifyChildren(noop)()
     },
     /Missing children in `parent`/,
@@ -14,6 +15,7 @@ test('modifyChildren()', function (t) {
 
   t.throws(
     function () {
+      // @ts-ignore runtime.
       modifyChildren(noop)({})
     },
     /Missing children in `parent`/,
@@ -21,15 +23,18 @@ test('modifyChildren()', function (t) {
   )
 
   t.test('should invoke `fn` for each child in `parent`', function (st) {
-    var values = [0, 1, 2, 3]
-    var context = {}
+    var children = [
+      {type: 'x', value: 0},
+      {type: 'x', value: 1},
+      {type: 'x', value: 2},
+      {type: 'x', value: 3}
+    ]
+    var context = {type: 'y', children}
     var n = -1
-
-    context.children = values
 
     modifyChildren(function (child, index, parent) {
       n++
-      st.strictEqual(child, values[n])
+      st.strictEqual(child, children[n])
       st.strictEqual(index, n)
       st.strictEqual(parent, context)
     })(context)
@@ -38,63 +43,109 @@ test('modifyChildren()', function (t) {
   })
 
   t.test('should work when new children are added', function (st) {
-    var values = [0, 1, 2, 3, 4, 5, 6]
+    var children = [
+      {type: 'x', value: 0},
+      {type: 'x', value: 1},
+      {type: 'x', value: 2},
+      {type: 'x', value: 3},
+      {type: 'x', value: 4},
+      {type: 'x', value: 5},
+      {type: 'x', value: 6}
+    ]
     var n = -1
 
     modifyChildren(function (child, index, parent) {
       n++
 
       if (index < 3) {
-        parent.children.push(parent.children.length)
+        parent.children.push({type: 'x', value: parent.children.length})
       }
 
-      st.strictEqual(child, values[n])
-      st.strictEqual(index, values[n])
-    })({children: [0, 1, 2, 3]})
+      st.deepEqual(child, children[n])
+      st.deepEqual(index, n)
+    })({
+      type: 'y',
+      children: [
+        {type: 'x', value: 0},
+        {type: 'x', value: 1},
+        {type: 'x', value: 2},
+        {type: 'x', value: 3}
+      ]
+    })
 
     st.end()
   })
 
   t.test('should skip forwards', function (st) {
-    var values = [0, 1, 2, 3]
+    var children = [
+      {type: 'x', value: 0},
+      {type: 'x', value: 1},
+      {type: 'x', value: 2},
+      {type: 'x', value: 3}
+    ]
     var n = -1
-    var context = {}
-
-    context.children = [0, 1, 3]
+    var context = {
+      type: 'y',
+      children: [
+        {type: 'x', value: 0},
+        {type: 'x', value: 1},
+        {type: 'x', value: 3}
+      ]
+    }
 
     modifyChildren(function (child, index, parent) {
-      st.strictEqual(child, values[++n])
+      st.deepEqual(child, children[++n])
 
-      if (child === 1) {
-        parent.children.splice(index + 1, 0, 2)
+      if (child.value === 1) {
+        parent.children.splice(index + 1, 0, {type: 'x', value: 2})
         return index + 1
       }
     })(context)
 
-    st.deepEqual(context.children, values)
+    st.deepEqual(context.children, children)
 
     st.end()
   })
 
   t.test('should skip backwards', function (st) {
-    var invocations = [0, 1, -1, 0, 1, 2, 3]
+    var calls = [
+      {type: 'x', value: 0},
+      {type: 'x', value: 1},
+      {type: 'x', value: -1},
+      {type: 'x', value: 0},
+      {type: 'x', value: 1},
+      {type: 'x', value: 2},
+      {type: 'x', value: 3}
+    ]
     var n = -1
-    var context = {}
-    var inserted
+    var context = {
+      type: 'y',
+      children: [
+        {type: 'x', value: 0},
+        {type: 'x', value: 1},
+        {type: 'x', value: 2},
+        {type: 'x', value: 3}
+      ]
+    }
+    var inserted = false
 
-    context.children = [0, 1, 2, 3]
+    modifyChildren(function (child, _, parent) {
+      st.deepEqual(child, calls[++n])
 
-    modifyChildren(function (child, index, parent) {
-      st.strictEqual(child, invocations[++n])
-
-      if (!inserted && child === 1) {
+      if (!inserted && child.value === 1) {
         inserted = true
-        parent.children.unshift(-1)
+        parent.children.unshift({type: 'x', value: -1})
         return -1
       }
     })(context)
 
-    st.deepEqual(context.children, [-1, 0, 1, 2, 3])
+    st.deepEqual(context.children, [
+      {type: 'x', value: -1},
+      {type: 'x', value: 0},
+      {type: 'x', value: 1},
+      {type: 'x', value: 2},
+      {type: 'x', value: 3}
+    ])
 
     st.end()
   })
